49bcb8b0aa9ba56ae9bf7def77576755cad1d1c6
GabrielAugustoMahl <gabrielmahl22@gmail.com>
2023-08-10 20:04:07 -0300

    Criando Calculadora

---
+++ Calculadora/src/main/java/com/devdojo/calculadora/Mult.java
@@ -0,0 +1,7 @@
+package com.devdojo.calculadora;
+
+public class Mult implements Calculo{
+    public double calcular(double pnum, double snum) {
+        return pnum * snum;
+    }
+}
---
+++ Calculadora/src/main/java/com/devdojo/calculadora/Sub.java
@@ -0,0 +1,7 @@
+package com.devdojo.calculadora;
+
+public class Sub implements Calculo{
+    public double calcular(double pnum, double snum) {
+        return pnum - snum;
+    }
+}
