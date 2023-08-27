b26cbbab40395e78e4884397e839d3afe8b2aac2
gustavo <tu-email@ejemplo.com>
2023-08-27 20:54:32 -0300

    calculadora com quarkus e qute

--- calculadoramvc/src/main/java/calculadoramvc/src/main/java/com/fag/CalculatorController.java
+++ calculadoramvc/src/main/java/calculadoramvc/src/main/java/com/fag/CalculatorController.java
@@ -1,8 +1,15 @@
 package calculadoramvc.src.main.java.com.fag;
-
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
-import jakarta.ws.rs.*;
+import jakarta.ws.rs.BadRequestException;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.FormParam;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 
 import static java.util.Objects.requireNonNull;
@@ -12,43 +19,13 @@ public class CalculatorController {
 
     private final Template calculator;
 
-    public CalculatorController (Template calculator) {
-        this.calculator = requireNonNull(calculator, "calculator is required");
+    public CalculatorController(Template calculator) {
+        this.calculator = requireNonNull(calculator, "page is required");
     }
 
     @GET
     @Produces(MediaType.TEXT_HTML)
-    public TemplateInstance get() {
-        return calculator.instance();
-    }
-
-    @POST
-    @Path("/calculate")
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    @Produces(MediaType.TEXT_HTML)
-
-    public TemplateInstance calculate(@FormParam("num1") double num1,
-                                      @FormParam("num2") double num2,
-                                      @FormParam("operation") String operation) {
-        double result = 0.0;
-
-        switch (operation) {
-            case "add":
-                result = num1 + num2;
-                break;
-            case "subtract":
-                result = num1 - num2;
-                break;
-            case "multiply":
-                result = num1 * num2;
-                break;
-            case "divide":
-                result = num2 != 0 ? num1 / num2 : Double.NaN;
-                break;
-            default:
-                throw new BadRequestException("Operação inválida");
-        }
-
-        return calculator.instance().data("result", result);
+    public TemplateInstance get(@QueryParam("name") String name) {
+        return calculator.data("name", name);
     }
 }
