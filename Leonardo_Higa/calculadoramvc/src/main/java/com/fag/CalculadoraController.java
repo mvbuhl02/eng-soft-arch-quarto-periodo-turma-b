acde1fc3286fc04d2d547f9c8e504442d959cc03
LeonardoHiga <lhgollub@minha.fag.edu.br>
2023-08-31 21:59:37 -0300

    csscalculadora

--- Leonardo_Higa/calculadoramvc/src/main/java/com/fag/CalculadoraController.java
+++ Leonardo_Higa/calculadoramvc/src/main/java/com/fag/CalculadoraController.java
@@ -6,31 +6,38 @@ import io.quarkus.qute.TemplateInstance;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.FormParam;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.POST;
 
 import static java.util.Objects.requireNonNull;
 
 @Path("/calculadora")
-public class SomePage {
+public class CalculadoraController {
 
     private final Template page;
 
-    public SomePage(Template page) {
+    public CalculadoraController(Template page) {
         this.page = requireNonNull(page, "page is required");
     }
 
     @GET
     @Produces(MediaType.TEXT_HTML)
-    public TemplateInstance get(
-            @QueryParam("name") String name,
-            @QueryParam("val1") Double valor1,
-            @QueryParam("val2") Double valor2) {
+    public TemplateInstance get() {
+        
+        return page.data("result", null);
+    }
+
+    @POST
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance calcular(
+            @FormParam("name") String name,
+            @FormParam("valor1") Double valor1,
+            @FormParam("valor2") Double valor2) {
         System.out.println(valor1);
         System.out.println(valor2);
         double result = valor1 + valor2;
 
         return page.data("result", result);
     }
-
 }
