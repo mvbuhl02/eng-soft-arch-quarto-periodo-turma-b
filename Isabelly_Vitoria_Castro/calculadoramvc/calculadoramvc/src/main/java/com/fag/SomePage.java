4df46e8ae6d5efa891c61da3f3ce93cee41dbc98
Isabelly Castro <isa.castroo0303@gmail.com>
2023-09-01 22:33:28 -0300

    Começo html

--- Isabelly_Vitoria_Castro/calculadoramvc/calculadoramvc/src/main/java/com/fag/SomePage.java
+++ Isabelly_Vitoria_Castro/calculadoramvc/calculadoramvc/src/main/java/com/fag/SomePage.java
@@ -2,11 +2,13 @@ package com.fag;
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
 
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
+
 
 import static java.util.Objects.requireNonNull;
 
@@ -15,17 +17,16 @@ public class SomePage {
 
     private final Template page;
 
+
     public SomePage(Template page) {
         this.page = requireNonNull(page, "page is required");
+        
     }
 
     @GET
     @Produces(MediaType.TEXT_HTML)
-    public TemplateInstance get(@QueryParam("val1") Double valor1,
-    @QueryParam("val2") Double valor2) {
-        System.out.println(valor1);
-        System.out.println(valor2);
-        return page.data("name", "IsinhaGameplays");
+    public TemplateInstance get() {
+        return page.data("dados",  "Isa");
     }
 
 }
