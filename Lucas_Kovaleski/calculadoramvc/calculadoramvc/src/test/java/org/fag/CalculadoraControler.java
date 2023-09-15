39a38a8e9bdd0453e1a8862acf9f0270db596a8f
lucaskovaleski <lucash.kovaleski@gmail.com>
2023-09-14 21:25:40 -0300

    calculadora MVC

--- Lucas_Kovaleski/calculadoramvc/calculadoramvc/src/test/java/org/fag/CalculadoraControler.java
+++ Lucas_Kovaleski/calculadoramvc/calculadoramvc/src/test/java/org/fag/CalculadoraControler.java
@@ -1,28 +1,29 @@
 package org.fag;
+
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
-
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 
 import static java.util.Objects.requireNonNull;
 
-@Path("/some-page")
-public class SomePage {
+@Path("calculadora")
+public class CalculadoraControler {
 
-    private final Template page;
+    private final Template calculadora;
 
-    public SomePage(Template page) {
-        this.page = requireNonNull(page, "page is required");
+    public CalculadoraControler(Template calculadora) {
+        this.calculadora = requireNonNull(calculadora, "page is required");
     }
 
     @GET
     @Produces(MediaType.TEXT_HTML)
     public TemplateInstance get(@QueryParam("name") String name) {
-        return page.data("name", name);
+    
+        return calculadora.data("name", name);
     }
 
 }
