2bb0d0bbde6c1ed1796f89362daf2aa962519393
Isabelly Castro <isa.castroo0303@gmail.com>
2023-09-15 20:03:34 -0300

    Calculadora Pronta

--- Isabelly_Vitoria_Castro/calculadoramvc/calculadoramvc/src/main/java/com/fag/SomePage.java
+++ Isabelly_Vitoria_Castro/calculadoramvc/calculadoramvc/src/main/java/com/fag/SomePage.java
@@ -2,14 +2,12 @@ package com.fag;
 
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
-import jakarta.inject.Inject;
+
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.UriBuilder;
 
 import static java.util.Objects.requireNonNull;
-
+ 
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,7 +17,6 @@ public class SomePage {
     private final Template page;
     private final Template resultTemplate;
 
-    @Inject
     public SomePage(Template page, Template result) { 
         this.page = requireNonNull(page, "page is required");
         this.resultTemplate = requireNonNull(result, "resultTemplate is required");
@@ -35,7 +32,6 @@ public class SomePage {
     @POST
     @Path("/calculate")
     @Produces(MediaType.TEXT_HTML)
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     public TemplateInstance calculate(
             @FormParam("numero1") double num1,
             @FormParam("numero2") double num2,
@@ -43,13 +39,13 @@ public class SomePage {
 
         double result = 0.0;
 
-        if ("add".equals(operation)) {
+        if ("somar".equals(operation)) {
             result = num1 + num2;
-        } else if ("subtract".equals(operation)) {
+        } else if ("subtrair".equals(operation)) {
             result = num1 - num2;
-        } else if ("multiply".equals(operation)) {
+        } else if ("multiplicar".equals(operation)) {
             result = num1 * num2;
-        } else if ("divide".equals(operation)) {
+        } else if ("dividir".equals(operation)) {
             if (num2 != 0) {
                 result = num1 / num2;
             } else {
