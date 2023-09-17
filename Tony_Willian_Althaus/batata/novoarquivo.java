21da0267edb1d6f204f3bbe988b772baba7e4edd
Tonuila <tonywillian400@gmail.com>
2023-09-17 03:01:17 -0300

    :wq

--- Tony_Willian_Althaus/batata/novoarquivo.java
+++
@@ -1,7 +0,0 @@
-public class novoarquivo {
-    public static void main(String[] args) {
-       int a;
-        System.out.println("Qual a operação? \n1 - a \n2 - subtrair \n3 - dividir \n4-multiplicar"); 
-    }
-    
-}
