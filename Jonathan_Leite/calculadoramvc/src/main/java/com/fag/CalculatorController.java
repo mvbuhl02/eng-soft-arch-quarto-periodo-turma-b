ca7b62e2392d5d55da425a6254cc1b7920a8b33c
JonnyAlf <jaleite@minha.fag.edu.br>
2023-09-18 21:29:51 -0300

    calculadoramvc

--- Jonathan_Leite/calculadoramvc/src/main/java/com/fag/CalculatorController.java
+++ Jonathan_Leite/calculadoramvc/src/main/java/com/fag/CalculatorController.java
@@ -3,6 +3,7 @@ import io.quarkus.qute.TemplateInstance;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import java.util.ArrayList;
 
 @Path("/calculadora")
 public class CalculatorController {
@@ -15,37 +16,44 @@ public class CalculatorController {
     @Produces(MediaType.TEXT_HTML)
     public TemplateInstance calculatorForm() {
         return calculatorTemplate.data("result", "")
-                                 .data("errorMessage", null);
+                                 .data("errorMessage", "");
     }
 
     @POST
-    @Path("/calculate")
     @Produces(MediaType.TEXT_HTML)
     public TemplateInstance calculate(
-        @FormParam("firstValue") double firstValue,
-        @FormParam("operation") String operation,
-        @FormParam("secondValue") double secondValue
+        @FormParam("valor01") double valor01,
+        @FormParam("operacao") String operacao,
+        @FormParam("valor02") double valor02
     ) {
         double result = 0.0;
         String errorMessage = null;
 
 
-       if (operation != null) {
-        if (operation.equals("add")) {
-            result = firstValue + secondValue;
-        } else if (operation.equals("subtract")) {
-            result = firstValue - secondValue;
-        } else if (operation.equals("multiply")) {
-            result = firstValue * secondValue;
-        } else if (operation.equals("divide")) {
-            if (secondValue == 0) {
+       if (operacao != null) {
+        if (operacao.equals("soma")) {
+            result = valor01 + valor02;
+        } else if (operacao.equals("subtrair")) {
+            result = valor01 - valor02;
+        } else if (operacao.equals("multiplicar")) {
+            result = valor01 * valor02;
+        } else if (operacao.equals("divide")) {
+            if (valor02 == 0) {
                 errorMessage = "Divisão por zero não é permitida.";
+               // result = null;
             } else{
-                result = firstValue / secondValue;
+                result = valor01 / valor02;
             } 
         }
        }
-       return calculatorTemplate.data("result", "Resultado: " + result)
-                                .data("errorMessage", errorMessage);
+
+       if (errorMessage != null) {
+            return calculatorTemplate.data("result", "Resultado: ERROR")
+                                     .data("errorMessage", errorMessage);
+               
+        } else{
+            return calculatorTemplate.data("result", "Resultado: " + result)
+                                     .data("errorMessage", errorMessage);
+        }  
     }
 }
