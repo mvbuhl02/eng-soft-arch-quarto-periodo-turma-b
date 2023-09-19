966d744a695d635b579427038a087b722c8bafbd
MuriloCastilho <murilo.tcastilho@gmail.com>
2023-09-18 22:15:04 -0300

    Calculadora bala

--- Gabriel_Augusto_Mahl/Calculadora/src/main/java/com/devdojo/calculadora/Calculadora.java
+++ Gabriel_Augusto_Mahl/Calculadora/src/main/java/com/devdojo/calculadora/Calculadora.java
@@ -9,44 +9,45 @@ public class Calculadora {
         while (dnv.equalsIgnoreCase("s")) {
             System.out.println("Digite o primeiro número: ");
 
-            Scanner scanner = new Scanner(System.in);
-            double pnum = scanner.nextDouble();
+            try (Scanner scanner = new Scanner(System.in)) {
+                double pnum = scanner.nextDouble();
 
-            System.out.println("Digite o segundo número: ");
-            double snum = scanner.nextDouble();
+                System.out.println("Digite o segundo número: ");
+                double snum = scanner.nextDouble();
 
-            System.out.println("Escolha a operação matemática (+,-,*,/): ");
-            String operador = scanner.next();
+                System.out.println("Escolha a operação matemática (+,-,*,/): ");
+                String operador = scanner.next();
 
-            Calculo operacao;
+                Calculo operacao;
 
-            switch (operador) {
-                case "+":
-                    operacao = new Soma();
-                    break;
-                    
-                case "-":
-                    operacao = new Sub();
-                    break;
-                    
-                case "*":
-                    operacao = new Mult();
-                    break;
-                    
-                case "/":
-                    operacao = new Div();
-                    break;
-                    
-                default:
-                    System.out.println("Operador inválido");
-                    return;
+                switch (operador) {
+                    case "+":
+                        operacao = new Soma();
+                        break;
+                        
+                    case "-":
+                        operacao = new Sub();
+                        break;
+                        
+                    case "*":
+                        operacao = new Mult();
+                        break;
+                        
+                    case "/":
+                        operacao = new Div();
+                        break;
+                        
+                    default:
+                        System.out.println("Operador inválido");
+                        return;
+                }
+                
+                double resultado = operacao.calcular(pnum, snum);
+                System.out.println("Resultado: " + resultado);
+                scanner = new Scanner(System.in);
+                System.out.println("Deseja realizar outra operação? (S/N)");
+                dnv = scanner.nextLine();
             }
-            
-            double resultado = operacao.calcular(pnum, snum);
-            System.out.println("Resultado: " + resultado);
-            scanner = new Scanner(System.in);
-            System.out.println("Deseja realizar outra operação? (S/N)");
-            dnv = scanner.nextLine();
             
         }
     }
